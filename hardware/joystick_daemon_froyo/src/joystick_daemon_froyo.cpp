8dde04f63f359e7c556ad2107db79fdf4147624c
Pedro Araujo <paraujo20700@gmail.com>
2026-04-10 11:19:14 -0300

    refatoração da função getJoystickState código atualizado igual ao compilado na v.1.0.0 do joystick

--- hardware/joystick_daemon_froyo/src/joystick_daemon_froyo.cpp
+++ hardware/joystick_daemon_froyo/src/joystick_daemon_froyo.cpp
@@ -7,32 +7,69 @@
 using aidl::com::android::joystick_froyo::BnJoystick;
 using ndk::ScopedAStatus;
 
-class JoystickService : public BnJoystick {
+class JoystickService : public BnJoystick
+{
 private:
-    JoystickHAL* joystick;
+    JoystickHAL *joystick;
+
 public:
-    JoystickService() {
+    JoystickService()
+    {
         joystick = new JoystickHAL();
-        if (joystick->init()) {
+        if (joystick->init())
+        {
             LOG(INFO) << "HAL do Joystick froyo inicializada com sucesso.";
-        } else {
+        }
+        else
+        {
             LOG(ERROR) << "Falha ao inicializar HAL do Joystick froyo.";
         }
     }
-    ~JoystickService() {
+    ~JoystickService()
+    {
         delete joystick;
     }
-    ScopedAStatus getJoystickState(int32_t* _aidl_return) override {
-        if (joystick) {
-            *_aidl_return = joystick->readJoystick();
-        } else {
-            *_aidl_return = -1;
+    ScopedAStatus getJoystickState(int32_t *_aidl_return) override
+    {
+        if (joystick)
+        {
+            int val = joystick->readJoystick();
+            *_aidl_return = val;
+
+            // Log formatado
+            std::string dir;
+            switch (val)
+            {
+            case 0x01:
+                dir = "UP";
+                break;
+            case 0x02:
+                dir = "LEFT";
+                break;
+            case 0x04:
+                dir = "DOWN";
+                break;
+            case 0x08:
+                dir = "PRESS";
+                break;
+            case 0x10:
+                dir = "RIGHT";
+                break;
+            case 0x00:
+                dir = "NONE";
+                break;
+            default:
+                dir = "UNKNOWN";
+                break;
+            }
+            LOG(INFO) << "Joystick: " << dir << " (0x" << std::hex << val << ")";
         }
         return ScopedAStatus::ok();
     }
 };
 
-int main() {
+int main()
+{
     android::base::InitLogging(nullptr, android::base::LogdLogger(android::base::SYSTEM));
     LOG(INFO) << "Iniciando daemon joystick_froyo...";
 
@@ -41,8 +78,9 @@ int main() {
 
     std::shared_ptr<JoystickService> service = ndk::SharedRefBase::make<JoystickService>();
     binder_status_t status = AServiceManager_addService(service->asBinder().get(), "joystick_froyo");
-    
-    if (status != STATUS_OK) {
+
+    if (status != STATUS_OK)
+    {
         LOG(ERROR) << "Falha ao registrar serviço joystick_froyo!";
         return -1;
     }
